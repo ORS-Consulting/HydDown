6a30cbdf95f3afd252a4b51b6373421ae03872ce
Anders Andreasen <58475535+andr1976@users.noreply.github.com>
2025-10-30 10:53:45 +0100

    Added Cd as input and default to emptying

--- scripts/streamlit_app.py
+++ scripts/streamlit_app.py
@@ -13,7 +13,7 @@ import os
 try:
     from hyddown import HydDown
 except:
-    
+
     hyddown_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "src")
     sys.path.append(os.path.abspath(hyddown_path))
     from hyddown import HydDown
@@ -53,9 +53,9 @@ def read_input():
             unsafe_allow_html=True,
         )
         st.write("")
-        #icon = Image.open(image_path)
-        #st.image(icon, width=100, caption="ORS Consulting HydDown App")
-        #st.logo(icon, size="large", link="http://www.ors-consulting.com", icon_image=None)
+        # icon = Image.open(image_path)
+        # st.image(icon, width=100, caption="ORS Consulting HydDown App")
+        # st.logo(icon, size="large", link="http://www.ors-consulting.com", icon_image=None)
 
         image_path = os.path.join(
             os.path.abspath(os.path.dirname(__file__)),
@@ -66,8 +66,7 @@ def read_input():
         )
         icon = Image.open(image_path)
         st.image(icon, use_container_width=True, caption="HydDown")
-        
-       
+
         heattran = st.checkbox("Include heat transfer", value=True)
         c1, c2 = st.columns(2)
 
@@ -75,32 +74,33 @@ def read_input():
             length = st.text_input("Vessel length (m):", 0.463)
             diam = st.text_input("Vessel diam (m):", 0.254)
             thk = st.text_input("Vessel thickness (m):", 0.016)
-            orientation = st.selectbox(
-                "Vessel orientation", ("horizontal", "vertical")
-            )
+            orientation = st.selectbox("Vessel orientation", ("horizontal", "vertical"))
+            tstep = st.text_input("Time step (s):", 1.0)
             orifice_diam = st.text_input("Orifice diam (mm):", 0.40)
             orifice_diam = float(orifice_diam) / 1000
-            tstep = st.text_input("Time step (s):", 1.0)
 
         with c1:
-            pres = st.text_input("Initial pressure (bar):", 50.0)
+            pres = st.text_input("Initial pressure (bar):", 350.0)
             pres = float(pres) * 1e5
 
-            back_pressure = st.text_input("Fill/back pres. (bar):", 240)
+            back_pressure = st.text_input("Fill/back pres. (bar):", 1)
             back_pressure = float(back_pressure) * 1e5
 
-            #fluid = st.selectbox('Select fluid', ('H2', 'He', 'N2', 'air', 'CH4', 'O2'))
-            fluid = st.selectbox('Select fluid', ('H2', 'NG', 'NG1', 'He', 'N2', 'air', 'CH4','O2'))
-            if fluid == 'NG':
+            # fluid = st.selectbox('Select fluid', ('H2', 'He', 'N2', 'air', 'CH4', 'O2'))
+            fluid = st.selectbox(
+                "Select fluid", ("H2", "NG", "NG1", "He", "N2", "air", "CH4", "O2")
+            )
+            if fluid == "NG":
                 fluid = "Methane[0.89571]&Ethane[5.6739e-02]&Propane[2.30395e-02]&Butane[1.03E-02]&Pentane[2.67E-03]&CO2[0.84e-02]&N2[0.3080e-2]"
-            if fluid == 'NG1':
+            if fluid == "NG1":
                 fluid = "Methane[0.860231]&Ethane[0.078217]&Propane[0.033786]&Butane[9.210E-03]&Pentane[2.573E-03]&Hexane[3.560E-04]&CO2[1.206E-02]&N2[3.701E-03]"
 
-            mode = st.selectbox('Select mode', ('filling', 'discharge'))
+            mode = st.selectbox("Select mode", ("discharge", "filling"))
             temp = st.text_input("Initial temp. (C):", 25)
             temp = float(temp) + 273.15
             end_time = st.text_input("End time (s):", 240)
 
+        Cd = st.text_input("Orifice discharge coefficient:", 0.84)
         density = st.text_input("Vessel material density (kg/m3):", 7740)
         density = float(density)
 
@@ -131,7 +131,7 @@ def read_input():
     input["valve"]["flow"] = mode
     input["valve"]["type"] = "orifice"
     input["valve"]["diameter"] = float(orifice_diam)
-    input["valve"]["discharge_coef"] = 0.84
+    input["valve"]["discharge_coef"] = float(Cd)
     input["valve"]["back_pressure"] = back_pressure
     # input['valve']['end_pressure']=end_pressure
 
@@ -156,16 +156,14 @@ if __name__ == "__main__":
     if st.sidebar.button("Run Simulation", type="primary"):
         with st.spinner("Calculating, please wait...."):
             hdown.run(disable_pbar=True)
-        
-        
-    
+
             df = hdown.get_dataframe()
             file_name = st.text_input("Filename for saving data:", "saved_data")
-        
+
             st.markdown(get_table_download_link(df, file_name), unsafe_allow_html=True)
-        
+
             col1, col2 = st.columns(2)
-        
+
             if input["valve"]["flow"] == "discharge":
                 temp_data = pd.DataFrame(
                     {
@@ -183,13 +181,13 @@ if __name__ == "__main__":
                         "Wall temperature (C)": hdown.T_vessel - 273.15,
                     }
                 )
-        
+
             pres_data = pd.DataFrame(
                 {"Time (s)": hdown.time_array, "Pressure (bar)": hdown.P / 1e5}
             )
-        
+
             fig, ax = plt.subplots(figsize=(5, 2))
-        
+
             ax.plot(
                 pres_data["Time (s)"],
                 pres_data["Pressure (bar)"],
@@ -198,14 +196,20 @@ if __name__ == "__main__":
             ax.set_xlabel("Time (s)")
             ax.set_ylabel("Pressure (bar)")
             col1.pyplot(fig)
-        
+
             fig, ax = plt.subplots(figsize=(5, 2))
-        
+
             ax.plot(
-                temp_data["Time (s)"], temp_data["Fluid temperature (C)"], "k", label="Fluid"
+                temp_data["Time (s)"],
+                temp_data["Fluid temperature (C)"],
+                "k",
+                label="Fluid",
             )
             ax.plot(
-                temp_data["Time (s)"], temp_data["Wall temperature (C)"], "k--", label="Wall"
+                temp_data["Time (s)"],
+                temp_data["Wall temperature (C)"],
+                "k--",
+                label="Wall",
             )
             if input["valve"]["flow"] == "discharge":
                 ax.plot(
@@ -214,19 +218,19 @@ if __name__ == "__main__":
                     "k-.",
                     label="Vent",
                 )
-        
+
             ax.set_xlabel("Time (s)")
             ax.set_ylabel("Temperature ($^\circ$C)")
             ax.legend(loc="best")
             col2.pyplot(fig)
-        
+
             mdot_data = pd.DataFrame(
                 {"Time (s)": hdown.time_array, "Mass rate (kg/s)": hdown.mass_rate}
             )
             mass_data = pd.DataFrame(
                 {"Time (s)": hdown.time_array, "Fluid inventory (kg)": hdown.mass_fluid}
             )
-        
+
             fig, ax = plt.subplots(figsize=(5, 2))
             ax.plot(
                 mdot_data["Time (s)"],
@@ -236,7 +240,7 @@ if __name__ == "__main__":
             ax.set_xlabel("Time (s)")
             ax.set_ylabel("Mass rate (kg/s)")
             col1.pyplot(fig)
-        
+
             fig, ax = plt.subplots(figsize=(5, 2))
             ax.plot(
                 mass_data["Time (s)"],
@@ -261,7 +265,6 @@ if __name__ == "__main__":
         
         **Key Features:**
         - **Real gas** vessel pressurisation/depressurisation with heat transfer from gas to vessel and ambient and vice versa. 
-        - Default Orifice discharge coefficient (Cd = 0.84) is specified for desired pressurisation/depressurisation rate.
         - **Dynamic simulation**: Models the time-dependent pressure and temperature development in fluid and vessel wall
         - **First law flow process**: Heat and mass balances solved concurrently 
         - **Heat transfer calculation**: Heat transfer between vessel wall and fluid using both natural and forced convection
